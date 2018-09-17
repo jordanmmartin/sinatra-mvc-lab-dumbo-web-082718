0374ee5c60e55f45f7292de282d8c779efacf24c
jordanmmartin <jdordan23@gmail.com>
2018-09-17 18:49:25 -0400

    Done.

--- app.rb
+++ app.rb
@@ -3,14 +3,13 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do
-    @text_from_user = params[:user_text]
-
     erb :user_input
   end
 
-  post '/' do
-    @analyzed_text = PigLatinizer.new(params[:user_text])
-    erb :piglatinize
+  post '/piglatinize' do
+    text = PigLatinizer.new
+    @analyzed_text = text.piglatinize(params[:user_phrase])
+    erb :piglatinizer
   end
 
 end
--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -3,20 +3,20 @@ require 'pry'
 class PigLatinizer
 
   VOWELS = ["a", "e", "i", "o", "u"]
-  DOUBLES = ["pl", "th", "pr"]
-  TRIPLES = ["spr"]
+  DOUBLES = ["pl", "th", "pr", "sk", "wh"]
+  TRIPLES = ["spr", "str"]
 
   def word_piglatinize(word)
     if VOWELS.include?(word[0].downcase)
       word + "way"
-  elsif DOUBLES.include?(word[0..1])
+    elsif DOUBLES.include?(word[0..1].downcase)
       word_array = word.split("")
         2.times do
           first_letter = word_array.shift
           word_array << first_letter
         end
         word_array.join + "ay"
-    elsif TRIPLES.include?(word[0..2])
+    elsif TRIPLES.include?(word[0..2].downcase)
         word_array = word.split("")
         3.times do
           first_letter = word_array.shift
